899cd2f4b974e5f6e6e2644cbd6a3d7f4baa375c
BananaLF <864685021@qq.com>
2019-05-01 01:04:38 +0800

    solve a bug when sendmany error. change once send money wait for pushing to block. add write remain addresses list file. add write has sent address list file.

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"encoding/csv"
 	"flag"
+	"github.com/btcsuite/btcd/chaincfg"
+	btcrpcclient "github.com/btcsuite/btcd/rpcclient"
+	"github.com/btcsuite/btcutil"
 	"io"
 	"io/ioutil"
 	"log"
@@ -10,11 +13,6 @@ import (
 	"strconv"
 	"strings"
 	"time"
-
-	"github.com/btcsuite/btcd/chaincfg"
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	btcrpcclient "github.com/btcsuite/btcd/rpcclient"
-	"github.com/btcsuite/btcutil"
 )
 
 type Result struct {
@@ -25,7 +23,8 @@ type Result struct {
 }
 
 var windows = 1000
-var path = "/Users/lifei"
+var path = "/Users/lifei/Desktop"
+var fileName = "/godAddress.csv"
 
 func main() {
 
@@ -34,11 +33,13 @@ func main() {
 	rpcaddr := flag.String("rpcaddr", "127.0.0.1:18886", "rpcaddr")
 	rpcwindows := flag.Int("windows", 1000, "windows")
 	isMainNet := flag.Bool("ismainnet", true, "ismainnet")
-	datadir := flag.String("datadir", "/Users/lifei", "datadir")
-
+	datadir := flag.String("datadir", "/Users/lifei/Desktop", "datadir")
+	datafile := flag.String("file", "/godAddress.csv", "godAddress.csv")
 	flag.Parse()
+
 	path = *datadir
 	windows = *rpcwindows
+	fileName = *datafile
 	// create new client instance
 	client, err := btcrpcclient.New(&btcrpcclient.ConnConfig{
 		HTTPPostMode: true,
@@ -60,12 +61,13 @@ func main() {
 	} else {
 		cfg = chaincfg.TestNet3Params
 	}
-
-	list, err := ReadCSV()
+	readFileName := path + fileName
+	list, err := ReadCSV(readFileName)
 	if err != nil {
 		log.Fatalf("error ReadCSV: %v", err)
 		return
 	}
+
 	length := len(list)
 	for start := 0; start < length; start += windows {
 		receivers := make(map[btcutil.Address]btcutil.Amount)
@@ -74,14 +76,30 @@ func main() {
 			end = length
 		}
 		if err := ConvertToMap(list[start:end], receivers, &cfg); err != nil {
+			remainFileName := path + "/remainAddress" + strconv.Itoa(start+1) + "-" + strconv.Itoa(length) + ".csv"
+			if err := WriteCSV(list[start:], remainFileName); err != nil {
+				log.Fatalf("error remainFileName WriteCSVTx: %v", err)
+				return
+			}
 			log.Fatalf("error ConvertToMap: %v", err)
 			return
 		}
-
+		log.Println("send address from:", strconv.Itoa(start+1), "  to:", strconv.Itoa(end))
 		if err := SendMany(client, receivers, &list, start, end); err != nil {
+			remainFileName := path + "/remainAddress" + strconv.Itoa(start+1) + "-" + strconv.Itoa(length) + ".csv"
+			if err := WriteCSV(list[start:], remainFileName); err != nil {
+				log.Fatalf("error remainFileName WriteCSVTx: %v", err)
+				return
+			}
 			log.Fatalf("error SendMany: %v", err)
 			return
 		}
+		hasSendFileName := path + "/hasSend" + strconv.Itoa(start+1) + "-" + strconv.Itoa(end) + ".csv"
+		if err := WriteCSV(list[start:end], hasSendFileName); err != nil {
+			log.Fatalf("error temptxid WriteCSVTx: %v", err)
+			return
+		}
+		log.Println("")
 	}
 
 	errAddress := 0
@@ -90,13 +108,14 @@ func main() {
 			errAddress += 1
 		}
 	}
-	log.Println("success address: %v,error address:%v", (len(list) - errAddress), errAddress)
-	if err := WriteCSVTx(list); err != nil {
-		log.Fatalf("error WriteCSVTx: %v", err)
+	log.Println("success send to addresses: %v,error address:%v", (len(list) - errAddress), errAddress)
+	tempTxidFileName := path + "/temptxid.csv"
+	if err := WriteCSV(list, tempTxidFileName); err != nil {
+		log.Fatalf("error temptxid WriteCSVTx: %v", err)
 		return
 	}
 
-	time.Sleep(time.Duration(150) * time.Second)
+	/*time.Sleep(time.Duration(150) * time.Second)
 	hasblock := make(map[string]string)
 	for i := range list {
 		for {
@@ -112,11 +131,12 @@ func main() {
 
 			txSha, err := chainhash.NewHashFromStr(list[i].txid)
 			if err != nil {
-				continue
+				break
 			}
 			txResult, err := client.GetTransaction(txSha)
 			if err != nil {
-				log.Fatalf("error GetTransaction: %v", err)
+				log.Println("error GetTransaction: %v", err)
+				time.Sleep(time.Duration(150) * time.Second)
 				continue
 			}
 			if txResult.Confirmations != 0 {
@@ -125,18 +145,95 @@ func main() {
 			}
 			time.Sleep(time.Duration(150) * time.Second)
 		}
-	}
+	}*/
 
-	if err := WriteCSV(list); err != nil {
-		log.Fatalf("error WriteCSV: %v", err)
+	resultFileName := path + "/result.csv"
+	if err := WriteCSV(list, resultFileName); err != nil {
+		log.Fatalf("error result WriteCSV: %v", err)
 		return
 	}
 }
 
-func ReadCSV() ([]Result, error) {
-	dat, err := ioutil.ReadFile(path + "/godAddress.csv")
+func ConvertToMap(list []Result, receivers map[btcutil.Address]btcutil.Amount, cfg *chaincfg.Params) error {
+	temp := make(map[string]btcutil.Amount)
+	for i := range list {
+		money, err := strconv.ParseFloat(list[i].amount, 64)
+		if err != nil {
+			log.Println("error read csv file convert amount int address:", list[i].String(), "--error:", err)
+			continue
+		}
+		amount, err := btcutil.NewAmount(money)
+		if err != nil {
+			log.Println("error read csv file convert amount float64 address:", list[i].String(), "--error:", err)
+			return err
+		}
+		if _, ok := temp[list[i].address]; ok {
+			temp[list[i].address] += amount
+		} else {
+			temp[list[i].address] = amount
+		}
+	}
+
+	for k, v := range temp {
+		receiver, err := btcutil.DecodeAddress(k, cfg)
+		if err != nil {
+			log.Println("error read csv file convert address:", k, "--error:", err)
+			continue
+		}
+
+		receivers[receiver] = v
+	}
+
+	return nil
+}
+
+func SendMany(client *btcrpcclient.Client, receivers map[btcutil.Address]btcutil.Amount, list *[]Result, start, end int) error {
+	// create and send the sendMany tx
+	txSha, err := client.SendMany("", receivers)
 	if err != nil {
-		log.Fatalf("error read csv file: %v", err)
+		log.Println("error sendMany:", err)
+		return err
+	}
+	//txSha := chainhash.HashH([]byte(string(start)))
+	for i := start; i < end; i++ {
+		for k, _ := range receivers {
+			if strings.Compare((*list)[i].address, k.String()) == 0 {
+				(*list)[i].txid = txSha.String()
+				break
+			}
+		}
+	}
+	log.Println("tx:", txSha.String())
+	log.Println("tx is waiting for pushing to block")
+	blockHash := "null"
+	//wait transaction is pushed into block
+	for {
+		txResult, err := client.GetTransaction(txSha)
+		if err != nil {
+			return err
+		}
+		if txResult.Confirmations != 0 {
+			blockHash = txResult.BlockHash
+			break
+		}
+		time.Sleep(time.Duration(10) * time.Second)
+	}
+	log.Println("block:", blockHash)
+	for i := start; i < end; i++ {
+		for k, _ := range receivers {
+			if strings.Compare((*list)[i].address, k.String()) == 0 {
+				(*list)[i].blockHash = blockHash
+				break
+			}
+		}
+	}
+	return nil
+}
+
+func ReadCSV(file string) ([]Result, error) {
+	dat, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Println("error read csv file: ", err)
 		return nil, err
 	}
 	r := csv.NewReader(strings.NewReader(string(dat[:])))
@@ -147,7 +244,7 @@ func ReadCSV() ([]Result, error) {
 			break
 		}
 		if err != nil {
-			log.Fatalf("error read csv file line: %v", err)
+			log.Println("error read csv file line:", err)
 			return nil, err
 		}
 		result := Result{address: record[1], amount: record[0]}
@@ -156,8 +253,8 @@ func ReadCSV() ([]Result, error) {
 	return list, nil
 }
 
-func WriteCSV(result []Result) error {
-	f, err := os.Create(path + "/testblock.csv") //创建文件
+func WriteCSV(result []Result, file string) error {
+	f, err := os.Create(file) //创建文件
 	if err != nil {
 		panic(err)
 		return err
@@ -181,84 +278,8 @@ func WriteCSV(result []Result) error {
 	return nil
 }
 
-func WriteCSVTx(result []Result) error {
-	f, err := os.Create(path + "/testtx.csv") //创建文件
-	if err != nil {
-		panic(err)
-		return err
-	}
-	defer f.Close()
-
-	f.WriteString("\xEF\xBB\xBF") // 写入UTF-8 BOM
-
-	w := csv.NewWriter(f) //创建一个新的写入文件流
-	data := make([][]string, 0)
-	for i := range result {
-		line := make([]string, 0)
-		line = append(line, result[i].amount)  //amount
-		line = append(line, result[i].address) //address
-		line = append(line, result[i].txid)    //txid
-		data = append(data, line)
-	}
-	w.WriteAll(data) //写入数据
-	w.Flush()
-	return nil
-}
-
-func ConvertToMap(list []Result, receivers map[btcutil.Address]btcutil.Amount, cfg *chaincfg.Params) error {
-	temp := make(map[string]btcutil.Amount)
-	for i := range list {
-		money, err := strconv.ParseFloat(list[i].amount, 64)
-		if err != nil {
-			log.Fatalf("error read csv file convert amount int: %v", err)
-		}
-		amount, err := btcutil.NewAmount(money)
-		if err != nil {
-			log.Fatalf("error read csv file convert amount float64: %v", err)
-		}
-		if _, ok := temp[list[i].address]; ok {
-			temp[list[i].address] += amount
-		} else {
-			temp[list[i].address] = amount
-		}
-	}
-
-	for k, v := range temp {
-		receiver, err := btcutil.DecodeAddress(k, cfg)
-		if err != nil {
-			continue
-			log.Fatalf("error read csv file convert address: %v", err)
-			return err
-		}
-
-		receivers[receiver] = v
-	}
-
-	return nil
-}
-
-func SendMany(client *btcrpcclient.Client, receivers map[btcutil.Address]btcutil.Amount, list *[]Result, start, end int) error {
-	// create and send the sendMany tx
-	txSha, err := client.SendMany("", receivers)
-	if err != nil {
-		log.Fatalf("error sendMany: %v", err)
-		return err
-	}
-	/*txSha := chainhash.HashH([]byte(string(start)))
-	for k,v := range receivers {
-		log.Printf("address:%s,amount%s", k.String(),v.String())
-	}*/
-	for i := start; i < end; i++ {
-		for k, _ := range receivers {
-			if strings.Compare((*list)[i].address, k.String()) == 0 {
-				(*list)[i].txid = txSha.String()
-				break
-			}
-		}
-	}
-	log.Printf("sendMany completed! tx sha is: %s", txSha.String())
-
-	return nil
+func (r *Result) String() string {
+	return "{" + "address:" + r.address + ",amount:" + r.amount + ",txid:" + r.txid + ",blockhash" + r.blockHash + "}"
 }
 
 /*receiver2, err := btcutil.DecodeAddress("gD9X8YdJdocfjdXEYxfhwHyPpr7ptPVxzU", &cfg)
